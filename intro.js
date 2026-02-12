29a24afc4c43a87b1a998aa4001b58fcf2b70513
Manuel Valentino Ortiz Sánchez <A01712296@tec.mx>
2026-02-12 12:54:56 -0600

    tipo de variables y avisos

--- intro.js
+++ intro.js
@@ -1,1 +1,41 @@
+//Consola de js
 console.log("Hola mundo!")
+console.info("Creado en el 2009")
+console.warn("Es adictivo")
+console.error("Los tanques no deben ir atrás")
+console.assert(1==true)
+
+//Variables
+//Antiguas tienen mayor alcance por lo que no se recomienda
+var personaje_1 = "Gwen"
+
+//Nuevas la variable solo vive dentro del ámbito donde es declarada
+let personaje_2 = "Mordekaiser"
+
+//Constantes
+const precio_skin = 300
+
+//Alcance de las variables
+{
+    var personaje_3 = "Jax"
+    let personaje_4 = "Garen"
+}
+
+console.log(personaje_3)
+//La siguiente linea genera un error porque personaje_4 murio hace 3 linea 
+//console.log(personaje_4)
+
+//Alert, prompt, confirm
+alert("No jueges esto porfavor")
+
+const personaje_favorito = prompt("¿Cuál es tu personaje favorito")
+console.info("Personaje favorito " + personaje_favorito) 
+
+const hoy_hay_juego = confirm("Hay juegito?")
+if(hoy_hay_juego){
+    console.warn("A jugar")
+}else{
+    console.info("Buen día")
+}
+
+//funciones tradicionales
