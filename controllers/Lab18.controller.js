2b47f9cbe45806c65a80b7dfac0e7a8e08289f07
Valentino Ortiz <A01712296@tec.mx>
2026-03-11 11:55:37 -0600

    Actualizar DB y nombres de archivos

--- controllers/Lab18.controller.js
+++ controllers/Lab18.controller.js
@@ -1,6 +1,6 @@
 exports.get_main = (request, response, next) => {
     response.render('index', {
-        titulo: "Inicio - Lab 14", 
+        titulo: "Inicio - Lab 18", 
         username: request.session.username || '',
         csrfToken: request.csrfToken(),
     });
--- sql/lab18.sql
+++ sql/lab18.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2026 at 12:02 AM
+-- Generation Time: Mar 11, 2026 at 06:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,29 @@ CREATE TABLE `passwords` (
 INSERT INTO `passwords` (`id`, `valor`, `fecha`) VALUES
 (1, '1234', '2026-03-07 22:14:32'),
 (2, 'hola', '2026-03-07 22:41:21'),
-(3, 'prueba´', '2026-03-07 22:41:24');
+(3, 'prueba´', '2026-03-07 22:41:24'),
+(4, 'ultima prueba', '2026-03-07 23:12:59'),
+(5, 'prueba con login', '2026-03-10 21:35:12'),
+(6, 'prueba', '2026-03-10 21:37:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `username` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`username`, `nombre`, `password`) VALUES
+('valentino123', 'Valentino', '$2b$12$1CMJDMgQd.GQCtcUGX9EYuUQT0terzCsaya5z4pxaE37VSL92GyRi');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +75,12 @@ ALTER TABLE `passwords`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,7 +88,7 @@ ALTER TABLE `passwords`
 -- AUTO_INCREMENT for table `passwords`
 --
 ALTER TABLE `passwords`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
